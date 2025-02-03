abef222010f076df71d5ca7fb10eab1e16d65745
Sivabalan Muthurajan <35603219+sivabalansm@users.noreply.github.com>
2025-02-03 22:43:07 +0000

    Hide sliders and and Show them

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
 import { randNum, randomSumProblem } from './mentalMathProblems.js';
 
+let options = document.getElementsByClassName('option');
+
+/*
+function toggleHide(element) {
+  if (element.style.display != "none") {
+    elememt.style.display = "none";
+  } else {
+    element.style.display = "block";
+  }
+}
+*/
+function matchCheckToRangeToggle(checkboxElement, display) {
+  switch (checkboxElement.name) {
+    case 'sums':
+      document.getElementById('sums-customize').style.display = display;
+      break;
+    case 'mult':
+      document.getElementById('mult-customize').style.display = display;
+      break;
+    case 'div':
+      document.getElementById('div-customize').style.display = display;
+      break;
+      
+  }
+}
+for (const optionElementCheckbox of options) {
+  optionElementCheckbox.addEventListener('change', function() {
+    if (optionElementCheckbox.checked) {
+      matchCheckToRangeToggle(optionElementCheckbox, 'block')
+    } else {
+    matchCheckToRangeToggle(optionElementCheckbox, 'none')
+    }
+  });
+
+if (optionElementCheckbox.checked) {
+    matchCheckToRangeToggle(optionElementCheckbox, 'block');
+}
+  else {
+    matchCheckToRangeToggle(optionElementCheckbox, 'none');
+  }
+}
