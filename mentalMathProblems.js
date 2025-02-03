069099d5ec4d1cc68b610a5fe26702f7c8dae734
Sivabalan Muthurajan <35603219+sivabalansm@users.noreply.github.com>
2025-02-03 01:15:51 +0000

    Finished Sum maker and started html modes

--- mentalMathProblems.js
+++ mentalMathProblems.js
@@ -1,3 +1,63 @@
+
+
+class ProblemMaker {
+  #problem;
+  #problemType;
+  constructor(problem, problemType) {
+    this.#problem = problem;
+    this.#problemType = problemType;
+  }
+  
+  get problem() {
+    return this.#problem;
+  }
+  get problemType() {
+    return this.#problemType;
+  }
+  
+  // Sum of numbers in array
+  static makeSum(arr) {
+    if (arr.length > 0)
+      return arr.reduce(function (total, new_addition) {
+        return total + new_addition;
+      });
+    return 0;
+  }
+  
+  static makeProduct(arr) {
+    return arr.reduce(function (total, new_multiplication) {
+      return total * new_multiplication;
+    });
+  }
+  
+  static makeDivison(arr) {
+    return arr.reduce(function (total, new_division) {
+      return total / new_division;
+    });
+  }
+  
+  make() {
+    let answer = 0;
+    switch (this.#problemType) {
+      case "a":
+      case "s":
+          answer = ProblemMaker.makeSum(this.#problem);
+          break;
+      case "m":
+          answer = ProblemMaker.makeProduct(this.#problem);
+          break;
+      case "d":
+          answer = ProblemMaker.makeDivison(this.#problem);
+          break;
+       default:
+          throw new Error("Unknown problemType: " + this.#problemType);
+    }
+    return answer;
+  }
+}
+
+
+// Random num generator with min and max
 function randNum(min, max) {
   
   if (!(max > min && max > 0 && min >= 0))
@@ -9,4 +69,25 @@ function randNum(min, max) {
 
 
 
-export { randNum };
+// New random Sum Problem 
+function randomSumProblem(min, max, isSubtraction, size) {
+  let sum = [];
+  
+  for (let numCount = 1; numCount <= size; numCount++) {
+    let newNum = 0;
+    let isCurrentlySubtraction = randNum(0, 1);
+    let sumOfNums = ProblemMaker.makeSum(sum);
+    if (sumOfNums > 0 && isSubtraction && isCurrentlySubtraction) {
+        newNum = -randNum(0, sumOfNums);
+    } else {
+        newNum = randNum(min, max);
+    }
+    sum.push(newNum)
+  }
+  return new ProblemMaker(sum, "s");
+}
+
+
+
+// Exports for other files
+export { randNum, randomSumProblem };
--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-import { randNum } from './mentalMathProblems.js';
+import { randNum, randomSumProblem } from './mentalMathProblems.js';
 
-for (let i = 0; i < 5; i++) {
-  console.log(randNum(0, 5))
-}
