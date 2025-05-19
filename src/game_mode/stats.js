8ccb057a0b69b402aaf9de1f99376ab20ec1a4c3
Sivabalan Muthurajan (Velup) <sivabalansm@yahoo.com>
2025-05-19 01:59:41 -0400

    Add stats calculation

--- src/game_mode/stats.js
+++ src/game_mode/stats.js
@@ -1,15 +1,104 @@
 
 class StatPoint {
+	// gameModeOption: { type: ..., option1: ..., option2: ... }
+	// problem: [...nums]
+	// time: seconds
+	problem;
+	time;
+	gameModeOption;
+
+	#getDigitsFromNum(num) {
+		return Math.floor(Math.log10(Math.abs(num)) + 1);
+	}
+
+	#decomposeNum(num) {
+		let decomp = [];
+		let exp = 0;
+		while (num > 0) {
+			decomp.push((num % 10) * (10 ** exp));
+			num = Math.floor(num / 10);
+			exp++;
+		}
+		return decomp;
+	}
+
+	#individualDigits(num) {
+		const decompNum = this.decompSecondNum(num);
+		for (let i = 0; i < decompNum.length; i++) {
+			decompNum[i] = parseInt(decompNum[i].toString()[0]);
+		}
+		return decompNum;
+	}
+
+
         constructor(gameModeOption, problem, time) {
+		this.gameModeOption = gameModeOption;
+		this.problem = problem;
+		this.time = time;
         }
 
-        ProblemPerSec() {
+	// getters
+	get type() {
+		return this.gameModeOption.type;
+	}
+
+	get time() {
+		return this.time;
+	}
+
+        problemPerSec() {
                 return 1/time;
         }
 
+	digitPerSec() {
+		let totalDigits = 0;
+		switch(gameModeOption.type) {
+			case 'sums':
+				// digit count divided by time
+				for (const num of this.problem) {
+					totalDigits += this.getDigitsFromNum(num);
+				}
+				break;
+
+			case 'mult':
+				const firstNum = problem[0];
+				const secondNum = problem[1];
+				
+				const firstNumDigits = this.getDigitsFromNum(firstNum);
+				const secondNumDigits = this.getDigitsFromNum(secondNum);
+
+				const everyMutlisADigitProcessed = firstNumDigits * secondNumDigits;
+
+				const decompSecondNum = this.decomposeNum(secondNum);
+
+				// count digits
+				totalDigits += everyMutlisADigitProcessed;
+				for (const decompNum of decompSecondNum) {
+					totalDigits += this.getDigitsFromNum(decompNum * firstNum);
+				}
+				break;
+
+			case 'div':
+				const firstNum = problem[0];
+				const secondNum = problem[1];
+				const answer = firstNum / secondNum;
+				const numberOfMults = this.getDigitsFromNum(answer);
+
+				totalDigits += numberOfMults;
+				
+				const individualDigitsAnswer = this.individualDigits(answer);
+
+				for (const answerDigit of individualDigitsAnswer) {
+					totalDigits += this.getDigitsFromNum(answerDigit * secondNum);
+				}
+				break;
+		}
+		return totalDigits/this.time;
+	}
+
 }
 class Stats {
-        pointList = [];
+        pointList = { 'sums' : [], 'mult' : [], 'div' : [] };
 
         // getters
         get pointList() {
@@ -17,24 +106,26 @@ class Stats {
 
         }
 
-
-        // setters
-
-        constructor() {
-
+        constructor(pointList = pointList) {
+		this.pointList = pointList;
         }
 
         addPoint(point) {
-                pointList.push(point);
+                pointList[point.type].push(point);
         }
 
         mean() {
+		const pointToDigitsPerSec = (num) => num.digitPerSec();
+		const average = (category) => ((pointList[category].map(pointToDigitsPerSec)).reduce((a, b) => a + b) / pointList[category].length)
+		return { 'sums' : average['sums'], 'mult' : average['mult'], 'div': average['div'] };
         }
 
         toJson() {
+		return JSON.stringify(this);
         }
 
-        fromJson() {
+        static fromJson(json) {
+		return new Stats(pointList = JSON.parse(json));
         }
 
 }
